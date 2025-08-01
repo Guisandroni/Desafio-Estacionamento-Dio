ffb9c4afb8ac4417173e623b832acede5e42819c
guisandroni <gui.sandroni@gmail.com>
2025-08-01 20:42:39 -0300

    feat: created desafio estacionamento

---
+++ DesafioFundamentos-Estacionamento-Dio/Models/Estacionamento.cs
@@ -0,0 +1,64 @@
+﻿namespace DesafioFundamentos_Estacionamento_Dio.Models;
+
+public class Estacionamento
+{
+    private List<string> veiculos = new List<string>();
+
+    private decimal PrecoInicial = 0;
+    private decimal PrecoPorHora = 0;
+
+    public Estacionamento(decimal PrecoInicial, decimal PrecoPorHora)
+    {
+        this.PrecoPorHora = PrecoPorHora;
+        this.PrecoInicial = PrecoInicial;
+    }
+
+    public void AddVeiculo()
+    {
+        Console.WriteLine("Informe a placa do veiculo");
+        
+        string placa = Console.ReadLine();
+        if (placa.Length > 7 && placa.Length < 1)
+        {
+            Console.WriteLine("Placa do veiculo invalida");
+        }
+        veiculos.Add(placa);
+
+        
+
+    }
+
+    public void RemoverVeiculo()
+    
+    {
+        Console.WriteLine("Informe a placa do veiculo!");
+        string placa = Console.ReadLine();
+
+        if (veiculos.Any(nomeVeiculo => nomeVeiculo.ToUpper() == placa.ToUpper()))
+        {
+            Console.WriteLine("Informe as horas que o veiculo permaneceu no estacionamento");
+            decimal valorEstacionamento;
+            decimal hora = decimal.Parse(Console.ReadLine());
+        
+            valorEstacionamento = PrecoPorHora * hora;
+            veiculos.Remove(placa);
+            Console.WriteLine($"Veiculo {placa} removido com sucesso valor a ser pago  R${valorEstacionamento}");
+        }
+        else
+        {
+            Console.WriteLine("Veiculo invalido , tente novamente");
+        }
+        
+       
+        
+    }
+
+    public void ListarVeiculos()
+    {
+        foreach (var i in veiculos)
+        {
+            Console.WriteLine("Listando Veiculos");
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ DesafioFundamentos-Estacionamento-Dio/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DesafioFundamentos_Estacionamento_Dio.Models;
+
+Estacionamento estacionamento = new Estacionamento(0,2);
+
+while (true)
+{
+    
+    
+    Console.WriteLine("\nSeja bem vindo ao estacionamento");
+    Console.WriteLine("1 - Adicionar veiculo");
+    Console.WriteLine("2 - Remover veiculo");
+    Console.WriteLine("3 - Listar veiculo");
+    Console.WriteLine("4 - Sair \n");
+    int option;
+
+    option = int.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+            estacionamento.AddVeiculo();
+            break;
+    
+        case 2 :
+            estacionamento.RemoverVeiculo();
+            break;
+        case 3 :
+            estacionamento.ListarVeiculos();
+            break;
+        default:
+            Environment.Exit(4);
+            break;
+    }
+    
+    
+}
